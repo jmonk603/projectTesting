11b249d4165e9b9d5dac123d9495fdeee78ca73d
jmonk603 <91668513+jmonk603@users.noreply.github.com>
2022-11-29 10:57:39 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -2,30 +2,41 @@ var y1,x1,mySong,amplitude;
 
 function preload(){
     mySong = loadSound('assets/massiveAttackAngel.mp3');
+    console.log('loaded');
 }
 
 function setup() {
     var canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.parent("p5container");
-    fill(0);
     stroke(255);
     y1 = window.innerHeight;
     x1 = window.innerWidth;
+    console.log(mySong);
+    mySong.play();
     amplitude = new p5.Amplitude();
+    
 }
 
 function draw() {
     let p  = [];
     f = frameCount;
     background(0);
+
+    textSize(65);
+    fill(255)
+    textAlign(CENTER, CENTER);
+    text('JOY DIVISION',(x1/2), 70);
+    textSize(37);
+    text('UNKNOWN PLEASURES',(x1/2), 940);
+
+    fill(0);
     var level = amplitude.getLevel();
 
-    var bbb = map(level, 0, 1500, 0, 350);
-
-    for (y = 70; y < y1; y += 10){
+    var bbb = map(level, 0, 1, 0, 150);
+    for (y = 200; y < 900; y += 10){
         beginShape();
-        for(x = 0; x < x1; x += 6){
-            dy = (bbb / (1 + pow(x - (x1/2), 4) / 8e6)) * noise(x / 30 + f / 50 + y);
+        for(x = 0; x < x1; x += 10){
+            dy = (bbb / (1 + pow(x - (x1/2), 4) / 8e6)) * noise(x / 30 + f / 60 + y);
             p.push({ x, y: y-dy });
             vertex(x, y-dy);
         }
